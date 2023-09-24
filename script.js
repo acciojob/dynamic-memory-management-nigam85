6ca619670f47bae2470c02c7a946c0103be643d8
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-24 11:06:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,18 +6,37 @@ let elements = []; // Array to hold generated DOM elements
 // Function to generate DOM elements
 const generateElements = () => {
   // complete the function
-  updateMemoryUsage();
-};
+	const container = document.getElementById('container');
+            for (let i = 0; i < 10000; i++) {
+                const element = document.createElement('div');
+                element.className = 'box';
+                container.appendChild(element);
+                elementsCount++;
+                memoryUsage += 0.0002; // Simulate memory usage (adjust this value as needed)
+                updateMemoryUsage();
+            };
+  
+
 
 // Function to remove DOM elements
 const removeElements = () => {
   // complete the function
-  updateMemoryUsage();
+	  const container = document.getElementById('container');
+            container.innerHTML = '';
+            elementsCount = 0;
+            memoryUsage = 0;
+            updateMemoryUsage();
 };
 
 // Function to update memory usage display
 const updateMemoryUsage = () => {
   // Complete this function
+	  const memoryUsageDisplay = document.getElementById('memoryUsage');
+            memoryUsageDisplay.textContent = `Memory Usage: ${memoryUsage.toFixed(2)} MB`;
+
+            if (memoryUsage > memoryLimit) {
+                alert("Memory usage has exceeded 50 MB. Please optimize your actions to reduce memory consumption.");
+            }
 };
 
 // Add event listeners to buttons
