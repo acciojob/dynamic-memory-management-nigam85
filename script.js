0f25cb7e952ee6553113b62e50e2fbdd5641b556
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-25 20:13:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
+// // Complete the js code
+
 const limit = 50; // Heap memory usage limit in MB
 let elements = []; // Array to hold generated DOM elements
 
 // Function to generate DOM elements
 const generateElements = () => {
   // complete the function
-	const container = document.getElementById('container');
-            for (let i = 0; i < 10000; i++) {
-                const element = document.createElement('div');
-                element.className = 'box';
-                container.appendChild(element);
-                elementsCount++;
-                memoryUsage += 0.0002; // Simulate memory usage (adjust this value as needed)
-                updateMemoryUsage();
-            };
-  
+  updateMemoryUsage();
+};
+
 // Function to remove DOM elements
 const removeElements = () => {
   // complete the function
-	  const container = document.getElementById('container');
-            container.innerHTML = '';
-            elementsCount = 0;
-            memoryUsage = 0;
-            updateMemoryUsage();
+  updateMemoryUsage();
 };
-	
-const updateMemoryUsage = () => {
-  
-	  const memoryUsageDisplay = document.getElementById('memoryUsage');
-            memoryUsageDisplay.textContent = `Memory Usage: ${memoryUsage.toFixed(2)} MB`;
 
-            if (memoryUsage > memoryLimit) {
-                alert("Memory usage has exceeded 50 MB. Please optimize your actions to reduce memory consumption.");
-            }
+// Function to update memory usage display
+const updateMemoryUsage = () => {
+  // Complete this function
 };
 
 // Add event listeners to buttons
